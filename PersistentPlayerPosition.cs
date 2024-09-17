f38f4f70b17d01c42bcd2156ab1c557459076758
alfuwu <130256600+alfuwu@users.noreply.github.com>
2024-09-16 23:53:04 -0700

    Initial commit

---
+++ PersistentPlayerPosition.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace PersistentPlayerPosition {
+	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
+	public class PersistentPlayerPosition : Mod {
+
+	}
+}
---
+++ PositionSavingPlayer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace PersistentPlayerPosition {
+    public class PositionSavingPlayer : ModPlayer {
+        private TagCompound loadedNBT;
+
+        public override void OnEnterWorld() {
+            Task.Run(async () => {
+                while (loadedNBT == null)
+                    await Task.Delay(1);
+                if (loadedNBT.TryGet("pos:" + Main.worldID + ":" + Main.worldName, out Vector2 pos))
+                    Player.position = pos;
+            });
+        }
+
+        public override void SaveData(TagCompound tag) {
+            if (loadedNBT != null)
+                foreach (KeyValuePair<string, object> kvp in loadedNBT)
+                    tag[kvp.Key] = kvp.Value;
+            if (Player.dead)
+                tag.Remove("pos:" + Main.worldID + ":" + Main.worldName);      
+            else
+                tag["pos:" + Main.worldID + ":" + Main.worldName] = Player.position;
+            //loadedNBT = null;
+        }
+
+        public override void LoadData(TagCompound tag) {
+            loadedNBT = tag;
+        }
+    }
+}
